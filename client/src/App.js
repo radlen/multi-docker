cd6811a7181ade809a328ff92a7bda8fe0620dbb
Radoslaw Lenik <rleni@2FJJBK3.localdomain>
2022-07-04 16:35:40 +0200

    changed displayed text

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactsss
+            Fibonacci calc
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
